e8d6df80367d5f7dfd0f5c758c5adfa35a7f54b2
Epicnessman9000 <Olivercfzhou@gmail.com>
2025-05-08 21:27:59 -0700

    Resource file for assetbundles
    Resource file for assetbundles, referenced in portrait modding guide 2025

---
+++ tutorials/tutorial-files/CreateAssetBundles.cs
@@ -0,0 +1,18 @@
+using UnityEditor;
+using UnityEngine;
+using System.IO;
+
+public class CreateAssetBundles : MonoBehaviour
+{
+    [MenuItem("Assets/Build AssetBundles")]
+    static void BuildAllAssetBundles()
+    {
+        string assetBundleDirectory = "Assets/StreamingAssets";
+        if (!Directory.Exists(Application.streamingAssetsPath))
+        {
+            Directory.CreateDirectory(assetBundleDirectory);
+        }
+        BuildPipeline.BuildAssetBundles(assetBundleDirectory, BuildAssetBundleOptions.None, EditorUserBuildSettings.activeBuildTarget);
+    }
+    
+}
